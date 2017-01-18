558373e0b30d6b955a2d7fc52e85684c976545d1
Nick Fenzan <nickf@millervein.com>
2017-01-18 09:52:10 -0500

    Initial commit

---
+++ src/main/java/com/millervein/clinicalordertracker/ClinicalordertrackerApplication.java
@@ -0,0 +1,12 @@
+package com.millervein.clinicalordertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalordertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalordertrackerApplication.class, args);
+	}
+}
